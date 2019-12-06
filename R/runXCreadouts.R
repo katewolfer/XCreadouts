3cfb5c7260e964d9690f11883b3a5f1f340b7898
Kate Wolfer <kate.wolfer@protonmail.com>
2019-12-06 15:49:25 +0100

    Update runXCreadouts.R

--- R/runXCreadouts.R
+++ R/runXCreadouts.R
@@ -3,11 +3,12 @@
 ## A small package for keeping an eye on your           ##
 ## Thermo Orbitrap instrument temperatures and vacuums  ##
 ## Kate Wolfer                                          ##
-## v 1.0, 05 Dec 2019                                   ##                
+## v 1.0, 05 Dec 2019                                   ##
 ##########################################################
 
 # import functions
-setwd("C:/Users/Kate/Dropbox/GitHub/XCreadouts")
+require(plotly)
+require(htmlwidgets)
 source("XCreadouts.R")
 source("XCplot.R")
 source("XCinteractive.R")
